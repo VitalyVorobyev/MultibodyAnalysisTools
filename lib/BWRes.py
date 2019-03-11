9b8f4793258e6f7489e3536fafed2e25bce22a4f
VitalyVorobyev <vit.vorobiev@gmail.com>
2019-03-11 10:36:02 +0700

    Breit-Wigner tutorial update

--- lib/BWRes.py
+++ lib/BWRes.py
@@ -9,7 +9,7 @@ from AngularDistribution import *
 from DalitzPhaseSpace import *
 
 def barier_factor(mom0, mom, spin):
-    """ Orbital Barier factor """
+    """ Orbital Barrier factor """
     return (mom / mom0) ** spin
 
 class BWRes(object):
@@ -19,17 +19,21 @@ class BWRes(object):
         self.prop = VarWidthRelBW(mass, width, spin, momentum)
         self.spin = spin
         self.momentum = momentum
+
     def __call__(self, mass_sq, momentum, cos_hel, mompq):
         """ Complex amplitude """
         return self.prop(mass_sq, momentum) * ang_dist(cos_hel, mompq, self.spin) *\
                barier_factor(self.momentum, momentum, self.spin)
+
     def __str__(self):
         """ to str """
         return 'RelBW: mass ' + str(np.sqrt(self.prop.mass_sq)) +\
                ', width ' +  str(self.prop.width.width)
+
     def set_mass(self, mass, momentum=None):
         """ Change mass """
         self.prop.set_mass(mass, momentum)
+
     def set_width(self, width):
         """ Change width """
         self.prop.set_width(width)
